2dcbf81131de30dcd9c9420e7877e3e4aab76e6a
GuangHongwei <guanghongwei@xiaomi.com>
2017-08-30 22:30:23 +0800

    修改一下bug

--- books/views.py
+++ books/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render, redirect
-
-
-from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 
 from .forms import PublisherForm
